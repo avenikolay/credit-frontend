0fa3c324188753ba588f67dc4d3377346d7d1df1
Николай Козлов <nikolay@velkomb.com>
2021-06-06 11:18:04 +0500

    feat: ordering by btns

--- src/components/CreditTable.vue
+++ src/components/CreditTable.vue
@@ -4,15 +4,25 @@
       <thead>
       <tr>
         <th>#</th>
-        <th>Номер кредита</th>
-        <th>Сумма ближайшего <br /> погашения</th>
-        <th>Общий остаток</th>
-        <th>Дата погашения</th>
+        <th>Номер кредита <button @click="sortBy = null; direction === 'ask' ? direction = 'desk' : direction = 'ask'" type="button" class="btn" :class="{'text-danger' : sortBy === null}"><i
+            class="bi bi-sort-down"></i></button></th>
+        <th>Сумма ближайшего погашения
+          <button @click="sortBy = 'actual'; direction === 'ask' ? direction = 'desk' : direction = 'ask'" type="button" class="btn" :class="{'text-danger' : sortBy === 'actual'}"><i
+              class="bi bi-sort-down"></i></button>
+        </th>
+        <th>Общий остаток
+          <button @click="sortBy = 'amount'; direction === 'ask' ? direction = 'desk' : direction = 'ask'" type="button" class="btn" :class="{'text-danger' : sortBy === 'amount'}"><i
+              class="bi bi-sort-down"></i></button>
+        </th>
+        <th>Дата погашения
+          <button @click="sortBy = 'date'; direction === 'ask' ? direction = 'desk' : direction = 'ask'" type="button" class="btn" :class="{'text-danger' : sortBy === 'date'}"><i
+              class="bi bi-sort-down"></i></button>
+        </th>
       </tr>
       </thead>
       <tbody>
-      <tr :key="credit.id" v-for="(credit, index) in sortedBy">
-        <td>{{index + 1}}</td>
+      <tr :key="credit.id" v-for="(credit, index) in orderedBy">
+        <td>{{ index + 1 }}</td>
         <td>{{ credit.number }}</td>
         <td>{{ credit.annuity_actual_graceful }}</td>
         <td>{{ credit.termination_amount }}</td>
@@ -34,15 +44,23 @@
 
 <script>
 import dayjs from 'dayjs'
-// eslint-disable-next-line no-unused-vars
+
 const sortByAmountSum = (a, b) => {
   return b.termination_amount - a.termination_amount
 }
-// eslint-disable-next-line no-unused-vars
 const sortByDate = (a, b) => {
   return (new Date(a.next_payment_date).getTime() - new Date(b.next_payment_date).getTime())
 }
+const sortByActual = (a, b) => {
+  return b.annuity_actual_graceful - a.annuity_actual_graceful
+}
 export default {
+  data() {
+    return {
+      sortBy: null,
+      direction: 'ask'
+    }
+  },
   props: {
     credits: {type: Array},
   },
@@ -54,8 +72,22 @@ export default {
       return this.credits.reduce((acc, curr) => Number((acc + parseFloat(curr.termination_amount)).toFixed(2)), 0)
     },
     sortedBy() {
-      //return [...this.credits].sort(sortByDate)
+      if (this.sortBy === 'amount') {
+        return [...this.credits].sort(sortByAmountSum)
+      }
+      if (this.sortBy === 'date') {
+        return [...this.credits].sort(sortByDate)
+      }
+      if (this.sortBy === 'actual') {
+        return [...this.credits].sort(sortByActual)
+      }
       return this.credits
+    },
+    orderedBy() {
+      if (this.direction === 'ask') {
+        return this.sortedBy.slice().reverse();
+      }
+      return this.sortedBy
     }
   },
   methods: {
@@ -66,3 +98,9 @@ export default {
   name: "CreditTable"
 }
 </script>
+<style>
+button:focus {
+  outline: none !important;
+  box-shadow: none !important;
+}
+</style>
